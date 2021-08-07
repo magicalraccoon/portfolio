335a7559ddefdf2a8b741ca4db6fad4e47215dd5
Sara Murray <sara@hackoon.com>
2021-08-07 02:36:37 -0700

    small update

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -32,7 +32,7 @@ const JobCard = styled.a`
 `
 
 const Portifolio = ({ className, location }) => {
-  const title = "Resume"
+  const title = "Portifolio"
   const { keywords, portifolio } = siteConfig
   return (
     <Layout location={location}>
